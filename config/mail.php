77e9cb4c8cce09611ed69643b2f65bc5fcd99fa3
yamazaki-yuki-23 <natural4083@gmail.com>
2019-10-20 11:36:59 +0900

    メール設定(ポート番号の変更)

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', null),
+        'address' => ('natural4083@gmail.com'),
         'name' => env('MAIL_FROM_NAME', null),
     ],
 
